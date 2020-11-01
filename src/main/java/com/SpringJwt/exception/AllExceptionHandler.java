bc094eea745cfa4d63450f868bd7263e95a443fa
857883 <kirankumarreddy.vulchakoti@gmail.com>
2020-11-01 12:29:44 +0530

    second commit added log files

--- src/main/java/com/SpringJwt/exception/AllExceptionHandler.java
+++ src/main/java/com/SpringJwt/exception/AllExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.SpringJwt.exception;
 
 import com.SpringJwt.models.ErrorMessage;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,8 +15,11 @@ import java.util.Date;
 @ControllerAdvice
 public class AllExceptionHandler extends ResponseEntityExceptionHandler {
 
+    private static final Logger logger= LoggerFactory.getLogger(AllExceptionHandler.class);
+
     public ResponseEntity<Object> handleRecordNotFoundException(ResourceNotFoundException ex, WebRequest request){
         ErrorMessage errorMessage=new ErrorMessage(new Date(),ex.getLocalizedMessage());
+        logger.info("Exception message with date"+errorMessage);
         return new ResponseEntity<>(new HttpHeaders(), HttpStatus.NOT_FOUND);
     }
 
